8fc7e63cf731b6433b4639b51513d5ec7bb61412
Nuclearfarts <Nuclearfarts@users.noreply.github.com>
2021-03-21 18:31:19 -0400

    door fix

--- src/main/java/paulevs/betternether/blocks/BlockBNDoor.java
+++ src/main/java/paulevs/betternether/blocks/BlockBNDoor.java
@@ -8,6 +8,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
@@ -42,7 +43,7 @@ public class BlockBNDoor extends BlockDoor
 	
 	public Item getItemDropped(IBlockState state, Random rand, int fortune)
     {
-        return Item.getItemFromBlock(this);
+        return state.getValue(HALF) == BlockDoor.EnumDoorHalf.UPPER ? Items.AIR : Item.getItemFromBlock(this);
     }
 	
 	public boolean canPlaceBlockAt(World world, BlockPos pos)
