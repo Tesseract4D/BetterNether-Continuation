05a163c77f06bb9629181ad9536db14e665c7c06
Nuclearfarts <Nuclearfarts@users.noreply.github.com>
2021-03-25 20:48:29 -0400

    eye fix 3: revengance: the coming of the eyes

--- src/main/java/paulevs/betternether/structures/plants/StructureEye.java
+++ src/main/java/paulevs/betternether/structures/plants/StructureEye.java
@@ -16,42 +16,43 @@ public class StructureEye implements IStructure
 	@Override
 	public void generate(World world, BlockPos pos, Random random)
 	{
-		int h = random.nextInt(19) + 5;
-		int h2 = pos.getY() - h;
-		if (h2 < 33)
-		{
-			return;
-		}
-		h2 = h;
-		boolean interrupted = false;
-		for (int y = 1; y < h; y++)
-		{
-			if (world.getBlockState(pos.down(y)).getBlock() != Blocks.AIR)
-			{
-				h2 = y;
-				interrupted = true;
-				break;
-			}
-		}
-		if (interrupted)
-		{
-			if (h2 < 5)
-				return;
-			h = 5 + random.nextInt(h2 / 3);
-		}
-		IBlockState vineState = BlocksRegister.BLOCK_EYE_VINE.getDefaultState();
-		IBlockState eyeState = null;
-		if (random.nextInt(2) == 0)
-			eyeState = BlocksRegister.BLOCK_EYEBALL.getDefaultState();
-		else
-			eyeState = BlocksRegister.BLOCK_EYEBALL_SMALL.getDefaultState();
 		if(ConfigLoader.isTerrain(world.getBlockState(pos.up()).getBlock())) {
+			int h = random.nextInt(19) + 5;
+			int h2 = pos.getY() - h;
+			if (h2 < 33)
+			{
+				return;
+			}
+			h2 = h;
+			boolean interrupted = false;
+			for (int y = 1; y < h; y++)
+			{
+				if (world.getBlockState(pos.down(y)).getBlock() != Blocks.AIR)
+				{
+					h2 = y;
+					interrupted = true;
+					break;
+				}
+			}
+			if (interrupted)
+			{
+				if (h2 < 5)
+					return;
+				h = 5 + random.nextInt(h2 / 3);
+			}
+			IBlockState vineState = BlocksRegister.BLOCK_EYE_VINE.getDefaultState();
+			IBlockState eyeState = null;
+			if (random.nextInt(2) == 0)
+				eyeState = BlocksRegister.BLOCK_EYEBALL.getDefaultState();
+			else
+				eyeState = BlocksRegister.BLOCK_EYEBALL_SMALL.getDefaultState();
+			
 			for (int y = 0; y < h; y++) {
 				world.setBlockState(pos.down(y), vineState);
 			}
-		}
 		
-		world.setBlockState(pos.down(h), eyeState);
+			world.setBlockState(pos.down(h), eyeState);
+		}
 	}
 
 }
